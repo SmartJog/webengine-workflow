c41936b61232fbef0265f0b3c6a2a00a7fe93ce2
Bryann Lamour <bryann.lamour@smartjog.com>
2011-05-27 14:08:04 +0200

    Organize javascript files

---
+++ medias/js/errors.js
@@ -0,0 +1,10 @@
+// Error messages
+var errorHappened = "An error unexpectedly happened. Would you like to update the page ?";
+var errorPageNotUpToDate = "Your workflow is not up to date. Would you like to update the page ?";
+
+// Title box
+var titleErrorPageNotUpToDate = "Page not up to date.";
+var titleErrorHappened = "Error unexpectedly happened.";
+
+// URLs
+var checkBaseURL = "/workflow/check/";
--- medias/js/shortcut.js
+++ medias/js/shortcut.js
@@ -1,32 +1,3 @@
-/* Compute failed/success/not_solved/mine item total */
-
-function compute_total_items_state() {
-    gl_not_solved = $("div.categories_table_workflow td.state-item-None").length;
-    gl_success = $("div.categories_table_workflow td.state-item-OK").length;
-    gl_failed = $("div.categories_table_workflow td.state-item-KO").length;
-    gl_total = gl_success + gl_failed + gl_not_solved;
-    update_statistics_progressbar();
-    $("#progress_bar").html(progressbar);
-    gl_total = gl_taken + gl_untaken;
-    update_statistics_filters();
-}
-
-/* ***************** */
-
-/* Compute taken/untaken item total */
-
-function compute_taken_untaken_items() {
-    var untaken_item_cel = $("div.categories_table_workflow").find(".category_workflow td.take-item");
-    var taken_item_cel = $("div.categories_table_workflow").find(".category_workflow td.untake-item");
-	var mine_item_cel = $("div.categories_table_workflow").find("td.owner-" + myID);
-    gl_taken = taken_item_cel.length;
-    gl_untaken = untaken_item_cel.length;
-	gl_mine = mine_item_cel.length;
-    update_statistics_filters();
-}
-
-/* ***************** */
-
 /* Update actions shortcut items */
 
 function  _update_item_shortcut(data, link, el) {
