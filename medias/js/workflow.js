3f19154adf4398d48f10a079d4888feb57252050
Bryann Lamour <bryann.lamour@smartjog.com>
2011-07-13 11:24:35 +0200

    Merge error.js in backbone_structure.js and rename it to workflow.js

--- medias/js/workflow.js
+++ medias/js/workflow.js
@@ -1,3 +1,31 @@
+function displayError(title, errorMessage) {
+   $('div#dialogError').attr('style', 'visibility: visible;');
+   $('div#dialogError').attr('title', title);
+   $('div#dialogError p').html(errorMessage);
+   $('div#dialogError').dialog({
+      modal    : true,
+      buttons  : {
+         Ok    : function () {
+            if (errorMessage === errorHappened) {
+                location.reload();
+            } else {
+                mainView._refreshPage();
+            }
+            $('div#dialogError').attr('style', 'visibility: hidden;');
+            $(this).dialog('close');
+         }
+      }
+   });
+}
+
+// Error messages
+var errorHappened = "An error happened unexpectedly. The page need to be reloaded.";
+var errorPageNotUpToDate = "The item you clicked on was not up to date, and has now been refreshed. Would you like to refresh remaining items ?";
+
+// Title box
+var titleErrorPageNotUpToDate = "Page not up to date.";
+var titleErrorHappened = "Error unexpectedly happened.";
+
 workflowItem = Backbone.Model.extend({
     initialize : function () {
         this.url = '/workflow/item/' + this.id + '/';
