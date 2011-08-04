5c4c93be185967dadd8e31067d8467cfbc817957
Bryann Lamour <bryann.lamour@smartjog.com>
2011-08-04 16:46:02 +0200

    Merge 'deleteItem()' and 'deleteCategory()' to '_delete()'

--- medias/js/workflow_admin.js
+++ medias/js/workflow_admin.js
@@ -32,61 +32,34 @@ function rename (el, elType) {
     });
 }
 
-function deleteItem(el) {
-    var itemLabel = $(el).find('td.label span').html();
-    var message = 'This action is irrecoverable !<br />Are you sure to delete <b>' + itemLabel + '</b> ?';
-    $('div#dialog-delete-item').removeClass('hidden').addClass('visible');
-    $('div#dialog-delete-item').find('p').html(message);
-    $('div#dialog-delete-item').dialog({
+function _delete(el, elType) {
+    var dialog = $('#dialog-delete');
+    var label = $(el).find('span.label').html();
+    var message = 'This action is irrecoverable !<br />Are you sure to delete <b>' + label + '</b> ?';
+    dialog.removeClass('hidden').addClass('visible');
+    dialog.find('p').html(message);
+    dialog.dialog({
+        open      : function() { dialog.parents('.ui-dialog-buttonpane button:eq(0)').focus(); },
+        title     : 'Delete ' + elType,
         resizable : false,
         modal     : true,
         buttons   : {
-            'Delete item': function() {
-                $('div#dialog-delete-item').find('p').html('Processing...');
-                var itemId = $(el).attr('id').match('\\d+$');
+            'Delete': function() {
+                dialog.find('p').html('Processing...');
+                var id = elType === 'category' ? $(el).parents('table').attr('id').match('\\d+$') : $(el).attr('id').match('\\d+$');
                 $.post(
                     '/workflow/delete/',
-                    JSON.stringify({'item_id' : parseInt(itemId)}),
+                    elType === 'category' ? JSON.stringify({'category_id' : parseInt(id)}) : JSON.stringify({'item_id' : parseInt(id)}),
                     function (data) {
-                        if ($(el).parents('table.category_workflow').find('tr.highlight').length === 1) {
+                        if (elType === 'item' && $(el).parents('table.category_workflow').find('tr.highlight').length === 1) {
                              $(el).parents('table.category_workflow').remove();
                         }
-                        $(el).remove();
-                        $('#dialog-delete-item').dialog('close');
+                        elType === 'category' ? $(el).parents('table').remove() : $(el).remove();
+                        dialog.dialog('close');
                     }
                 );
             },
-            'Cancel': function() {
-                $(this).dialog('close');
-            }
-        }
-    });
-}
-
-function deleteCategory(el) {
-    var categoryLabel = $(el).find('th span').html();
-    var message = 'This action is irrecoverable !<br />Are you sure to delete <b>' + categoryLabel + '</b> ?';
-    $('div#dialog-delete-category').removeClass('hidden').addClass('visible');
-    $('div#dialog-delete-category').find('p').html(message);
-    $('div#dialog-delete-category').dialog({
-        resizable : false,
-        modal     : true,
-        buttons   : {
-            'Delete item': function() {
-                $('div#dialog-delete-category').find('p').html('Processing...');
-                var categoryId = $(el).parents('table').attr('id').match('\\d+$');
-                $.post(
-                    '/workflow/delete/',
-                    JSON.stringify({'category_id' : parseInt(categoryId)}),
-                    function (data) {
-                        $(el).parents('table').remove();
-                        $('#dialog-delete-category').dialog('close');
-                    }
-                );
-            },
-           'Cancel': function() {
-               $(this).dialog('close');
-            }
+           'Cancel': function() { dialog.dialog('close'); }
         }
     });
 }
